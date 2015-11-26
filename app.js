8ea336c425248ef8f3d8c884f0471e6aa7a8aa03
iMing621 <ming0621@gmail.com>
2015-11-26 13:57:56 +0800

    update

--- app.js
+++ app.js
@@ -111,61 +111,20 @@ Ext.create('Ext.FormPanel', {
 					handler: function(btn, evt){
 						//console.debug(Ext.getCmp('rg-fromstation').getChecked()[0].inputValue);
 						//console.debug(Ext.getCmp('searchdate').getValue());
-						getURL();
+						doQuery();
 					}
 				}]
 		});
-
-
-/*Ext.create('Ext.form.Panel', {
-    title: 'Simple Form with FieldSets',
-    labelWidth: 75, // label settings here cascade unless overridden
-    url: 'save-form.php',
-    frame: true,
-    bodyStyle: 'padding:5px 5px 0',
-    width: 550,
-    renderTo: Ext.getBody(),
-    layout: 'column', // arrange fieldsets side by side
-    defaults: {
-        bodyPadding: 4
-    },
-    items: [{
-        // Fieldset in Column 1 - collapsible via toggle button
-        xtype:'fieldset',
-        columnWidth: 0.5,
-        title: 'Fieldset 1',
-        collapsible: true,
-        defaultType: 'textfield',
-        defaults: {anchor: '100%'},
-        layout: 'anchor',
-        items :[{
-            fieldLabel: 'Field 1',
-            name: 'field1'
-        }, {
-            fieldLabel: 'Field 2',
-            name: 'field2'
-        }]
-    }, {
-        // Fieldset in Column 2 - collapsible via checkbox, collapsed by default, contains a panel
-        xtype:'fieldset',
-        title: 'Show Panel', // title or checkboxToggle creates fieldset header
-        columnWidth: 0.5,
-        checkboxToggle: true,
-        collapsed: true, // fieldset initially collapsed
-        layout:'anchor',
-        items :[{
-            xtype: 'panel',
-            anchor: '100%',
-            title: 'Panel inside a fieldset',
-            frame: true,
-            height: 52
-        }]
-    }]
-});*/
-
 });
 
-function getURL() {
+function lpad(str) {
+	if (str < 10)
+		return '0' + str;
+	else
+		return str;
+}
+
+function doQuery() {
 	var url = "http://twtraffic.tra.gov.tw/twrail/SearchResult.aspx?searchtype=0";
 	var fromstation  = Ext.getCmp('rg-fromstation').getChecked()[0].inputValue;
 	var tostation    = Ext.getCmp('rg-tostation').getChecked()[0].inputValue;
@@ -174,7 +133,7 @@ function getURL() {
 	var totime       = Ext.getCmp('rg-totime').getChecked()[0].inputValue;
 
 	var params = 
-		'&searchdate='  + searchdate.getFullYear() + '/' + ((searchdate.getMonth() +1)>9)? '':'0' + (searchdate.getMonth() +1) + '/' +(searchdate.getDate()>9)? '':'0' +  searchdate.getDate() + 
+		'&searchdate='  + searchdate.getFullYear() + '/' + lpad(searchdate.getMonth() +1) + '/' + lpad(searchdate.getDate()) + 
 		'&fromstation=' + fromstation +
 		'&tostation='   + tostation +
 		'&trainclass=2&timetype=1' + 
