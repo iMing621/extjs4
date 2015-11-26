fc8a414223d369dac48891cda13c72f98d45fb6f
iMing621 <ming0621@gmail.com>
2015-11-26 15:18:53 +0800

    test

--- app.js
+++ app.js
@@ -140,24 +140,112 @@ function doQuery() {
 
 	//window.open(url + params, 100, 100);
 
-	Ext.Ajax.request({
-		url: url + params,
-		method: 'GET',
-		timeout: 60000,
-/*		params:
-		{
-			id: 1 // loads student whose Id is 1
-		},*/
-/*		headers:
-		{
-			'Content-Type': 'application/json'
-		},*/
-		success: function (response) {
-			Ext.getDom('center').innerHTML = response.responseText;
-		},
-		failure: function (response) {
-			Ext.Msg.alert('Status', 'Request Failed.');
+	var myData = {
+	    "users": [
+	        {
+	            "id": 123,
+	            "name": "Ed",
+	            "orders": [
+	                {
+	                    "id": 50,
+	                    "total": 100,
+	                    "order_items": [
+	                        {
+	                            "id"      : 20,
+	                            "price"   : 40,
+	                            "quantity": 2,
+	                            "product" : {
+	                                "id": 1000,
+	                                "name": "MacBook Pro"
+	                            }
+	                        },
+	                        {
+	                            "id"      : 21,
+	                            "price"   : 20,
+	                            "quantity": 3,
+	                            "product" : {
+	                                "id": 1001,
+	                                "name": "iPhone"
+	                            }
+	                        }
+	                    ]
+	                }
+	            ]
+	        }
+	    ]
+	};
 
-		}
+
+	Ext.define("User", {
+	    extend: 'Ext.data.Model',
+	    fields: [
+	        'id', 'name'
+	    ],
+
+	    hasMany: {model: 'Order', name: 'orders'},
+
+	    proxy: {
+	        type: 'rest',
+	        url : myData,
+	        reader: {
+	            type: 'json',
+	            root: 'users'
+	        }
+	    }
+	});
+
+	Ext.define("Order", {
+	    extend: 'Ext.data.Model',
+	    fields: [
+	        'id', 'total'
+	    ],
+
+	    hasMany  : {model: 'OrderItem', name: 'orderItems', associationKey: 'order_items'},
+	    belongsTo: 'User'
+	});
+
+	Ext.define("OrderItem", {
+	    extend: 'Ext.data.Model',
+	    fields: [
+	        'id', 'price', 'quantity', 'order_id', 'product_id'
+	    ],
+
+	    belongsTo: ['Order', {model: 'Product', associationKey: 'product'}]
+	});
+
+	Ext.define("Product", {
+	    extend: 'Ext.data.Model',
+	    fields: [
+	        'id', 'name'
+	    ],
+
+	    hasMany: 'OrderItem'
+	});
+
+	var store = Ext.create('Ext.data.Store', {
+	    model: "User"
+	});
+
+	store.load({
+	    callback: function() {
+	        //the user that was loaded
+	        var user = store.first();
+
+	        console.log("Orders for " + user.get('name') + ":")
+
+	        //iterate over the Orders for each User
+	        user.orders().each(function(order) {
+	            console.log("Order ID: " + order.getId() + ", which contains items:");
+
+	            //iterate over the OrderItems for each Order
+	            order.orderItems().each(function(orderItem) {
+	                //we know that the Product data is already loaded, so we can use the synchronous getProduct
+	                //usually, we would use the asynchronous version (see Ext.data.association.BelongsTo)
+	                var product = orderItem.getProduct();
+
+	                console.log(orderItem.get('quantity') + ' orders of ' + product.get('name'));
+	            });
+	        });
+	    }
 	});
 }
